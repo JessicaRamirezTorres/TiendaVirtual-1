d91b6e0f8bc3bef1f9ad29e0a6875313f782af71
JessicaRamirezT <111193935+JessicaRamirezT@users.noreply.github.com>
2022-10-13 11:21:24 -0500

    comunicación con localhost:3000
    adición de logo, nombre y preparación del front

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Tienda Emplas: En Armonía con el Planeta</h1>
+        <img src="./images/logoEmplas.png" alt="Aqui va el logo Principal"></img>
     </div>
   );
 }
